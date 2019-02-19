480537f0bc045b25eec8d615e94b389bce138184
kev.dev <kevynkybet@gmail.com>
2019-02-19 03:36:00 +0300

    Initial commit

---
+++ MPesaGatewayFactory.php
@@ -0,0 +1,47 @@
+<?php
+namespace Payum\Mpesa;
+
+use Payum\Mpesa\Action\AuthorizeAction;
+use Payum\Mpesa\Action\CancelAction;
+use Payum\Mpesa\Action\ConvertPaymentAction;
+use Payum\Mpesa\Action\CaptureAction;
+use Payum\Mpesa\Action\NotifyAction;
+use Payum\Mpesa\Action\RefundAction;
+use Payum\Mpesa\Action\StatusAction;
+use Payum\Core\Bridge\Spl\ArrayObject;
+use Payum\Core\GatewayFactory;
+
+class MPesaGatewayFactory extends GatewayFactory
+{
+    /**
+     * {@inheritDoc}
+     */
+    protected function populateConfig(ArrayObject $config)
+    {
+        $config->defaults([
+            'payum.factory_name' => 'mpesa',
+            'payum.factory_title' => 'Mpesa',
+            'payum.action.capture' => new CaptureAction(),
+            'payum.action.authorize' => new AuthorizeAction(),
+            'payum.action.refund' => new RefundAction(),
+            'payum.action.cancel' => new CancelAction(),
+            'payum.action.notify' => new NotifyAction(),
+            'payum.action.status' => new StatusAction(),
+            'payum.action.convert_payment' => new ConvertPaymentAction(),
+        ]);
+
+        if (false == $config['payum.api']) {
+            $config['payum.default_options'] = array(
+                'sandbox' => true,
+            );
+            $config->defaults($config['payum.default_options']);
+            $config['payum.required_options'] = [];
+
+            $config['payum.api'] = function (ArrayObject $config) {
+                $config->validateNotEmpty($config['payum.required_options']);
+
+                return new Api((array) $config, $config['payum.http_client'], $config['httplug.message_factory']);
+            };
+        }
+    }
+}
